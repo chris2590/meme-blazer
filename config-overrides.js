257acdd021563ea67bae67be79088d3045cef76d
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-20 17:00:36 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,8 @@ const webpack = require('webpack');
 
 module.exports = function override(config) {
   config.resolve.fallback = {
-    ...config.resolve.fallback,
-    "http": require.resolve("stream-http")
+    "http": require.resolve("stream-http"),
+    "zlib": require.resolve("browserify-zlib")
   };
   return config;
 };
