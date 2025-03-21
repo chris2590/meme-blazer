38ca7452aca94e59ef703ead37c1d0a0ab831806
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-21 01:10:59 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,24 +1,30 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
+  // Preserve any existing fallbacks, if they exist, or start with an empty object
   const fallback = config.resolve.fallback || {};
+  
+  // Assign all necessary module fallbacks
   Object.assign(fallback, {
+    "vm": require.resolve("vm-browserify"),
     "http": require.resolve("stream-http"),
     "zlib": require.resolve("browserify-zlib"),
     "https": require.resolve("https-browserify"),
     "url": require.resolve("url/"),
     "stream": require.resolve("stream-browserify"),
-    "crypto": require.resolve("crypto-browserify"),
-    "vm": require.resolve("vm-browserify"),
-    "buffer": require.resolve("buffer/"),
-    "process": require.resolve("process/browser")
+    "crypto": require.resolve("crypto-browserify")
   });
+  
+  // Apply the updated fallbacks to the config
   config.resolve.fallback = fallback;
+  
+  // Add ProvidePlugin to make 'process' and 'Buffer' available globally
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
     }),
   ]);
+  
   return config;
 };
