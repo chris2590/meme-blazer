e27404b8d99e27e8a4deadc9e5b01bf15b349c58
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-20 22:09:04 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,8 @@ module.exports = function override(config) {
     "zlib": require.resolve("browserify-zlib"),
     "https": require.resolve("https-browserify"),
     "url": require.resolve("url/"),
-    "stream": require.resolve("stream-browserify")
+    "stream": require.resolve("stream-browserify"),
+    "crypto": require.resolve("crypto-browserify")
   };
   return config;
 };
