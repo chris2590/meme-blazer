44d19ed1f64edb5f325395ddd47a162066ac00a7
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-23 20:43:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
     outDir: 'dist',
     sourcemap: true
   },
