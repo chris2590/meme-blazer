b3ef67151df7f3e643fd5e6c70fd1a44cd21b404
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-23 21:24:41 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MemeBlazer from "./MemeBlazer.jsx";
+import MemeBlazer from "./App.js"; // <- FIXED RIGHT HERE
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
