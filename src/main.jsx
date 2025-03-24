6e17eadfac5adedf3ab531de45d98453cc4982e4
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-24 16:39:55 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MemeBlazer from "./App";
+import App from "./App.jsx";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
+
+const wallets = [new PhantomWalletAdapter()];
+const endpoint = clusterApiUrl("mainnet-beta");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MemeBlazer />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>
 );
