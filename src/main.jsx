a42c3ee9348a1129062203c1e9b016e1c7fd221e
chris2590 <99231439+chris2590@users.noreply.github.com>
2025-03-24 14:23:48 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MemeBlazer from "./App.jsx";
+import MemeBlazer from "./App";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MemeBlazer />
   </React.StrictMode>
